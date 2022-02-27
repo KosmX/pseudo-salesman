1a4cf9adc98cdeaacd1b124f6494c2fe6a9e8491
KosmX <kosmx.mc@gmail.com>
2022-02-27 11:56:39 +0100

    save the file...

--- src/main/kotlin/dev/kosmx/lowPassSimulator/CArrayUtil.kt
+++ src/main/kotlin/dev/kosmx/lowPassSimulator/CArrayUtil.kt
@@ -7,39 +7,41 @@ import java.nio.file.Paths
 import java.util.InputMismatchException
 import java.util.Scanner
 import java.util.regex.Pattern
+import kotlin.math.roundToInt
 
 fun getDataFromFile(filename:String): List<Pos> {
     val coords = mutableListOf<Pos>()
     try {
-        val file = Files.newBufferedReader(Paths.get(filename))
-        val scanner = Scanner(file)
+        Files.newBufferedReader(Paths.get(filename)).use {
+            val scanner = Scanner(it)
 
-        val lineBeginPattern = Pattern.compile(" *int\\s*\\w*\\[]\\s*=\\s*\\{\\s*")
+            val lineBeginPattern = Pattern.compile(" *int\\s*\\w*\\[]\\s*=\\s*\\{\\s*")
 
-        // Read the #include, comment lines before the string
-        while (scanner.findInLine(lineBeginPattern) == null) {
-            scanner.nextLine()
-        }
-        //We did find the line, now read the beginning before the numbers
-        scanner.findInLine(lineBeginPattern)
+            // Read the #include, comment lines before the string
+            while (scanner.findInLine(lineBeginPattern) == null) {
+                scanner.nextLine()
+            }
+            //We did find the line, now read the beginning before the numbers
+            scanner.findInLine(lineBeginPattern)
 
-        while(true) {
-            try {
-                val pattern = Pattern.compile("\\s*,\\s*")
-                //comma deliminator
-                scanner.useDelimiter(pattern)
+            while (true) {
+                try {
+                    val pattern = Pattern.compile("\\s*,\\s*")
+                    //comma deliminator
+                    scanner.useDelimiter(pattern)
 
-                val x = scanner.nextInt()
+                    val x = scanner.nextInt()
 
-                if (!scanner.hasNextInt()) break //Why scanner?!
-                val y = scanner.nextInt()
+                    if (!scanner.hasNextInt()) break //Why scanner?!
+                    val y = scanner.nextInt()
 
-                coords.add(Pos(x.toDouble(), y.toDouble())) //Add the new coordinate to our set
+                    coords.add(Pos(x.toDouble(), y.toDouble())) //Add the new coordinate to our set
 
-                if (!scanner.hasNextInt()) break
-            } catch (e: InputMismatchException) {
-                e.printStackTrace()
-                break
+                    if (!scanner.hasNextInt()) break
+                } catch (e: InputMismatchException) {
+                    e.printStackTrace()
+                    break
+                }
             }
         }
 
@@ -58,11 +60,14 @@ fun writeCArray(posIt: Iterator<Pos>, file: Path) {
 
         while (posIt.hasNext()) {
             val pos = posIt.next()
-            writer.write("${pos.x}, ${pos.y}")
+            val x = pos.x.roundToInt()
+            val y = pos.y.roundToInt()
+            writer.write("${x}, ${y}")
 
             if (posIt.hasNext()) writer.write(", ")
         }
         writer.write("};")
+        writer.close()
 
     } catch (e: Throwable) {
         e.printStackTrace()
